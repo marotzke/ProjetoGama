c73ed92266b69bb54184307cefe2a2d0697cb405
Gabriel Zsigmond <gaazsig@gmail.com>
2020-04-27 15:09:56 -0300

    video autoplays and pauses according to its visibility

--- js/autoPlayMp4.js
+++ js/autoPlayMp4.js
@@ -1,8 +1,20 @@
 const playMp4 = () => {
   let vid = document.querySelector("#teaser-vid");
+  if (vid.hasAttribute("data-played", true)) {
+    vid.muted = true;
+  }
   if (vid.autoplay === false) {
     vid.autoplay = true;
     vid.load();
+  }
+};
+
+const pauseMp4 = () => {
+  let vid = document.querySelector("#teaser-vid");
+  if (vid.autoplay === true) {
+    vid.autoplay = false;
+    vid.load();
+    vid.setAttribute("data-played", true);
   }
 };
 
@@ -10,6 +22,8 @@ let observer = new IntersectionObserver(
   function (entries) {
     if (entries[0].isIntersecting === true) {
       playMp4();
+    } else {
+      pauseMp4();
     }
   },
   { threshold: [1] }
